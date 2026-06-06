d9d26511b3f5cb07ac1b1bd1136b1ac24bb0eb18
Ankit Kumar <ankit.developer2004@gmail.com>
2026-06-06 09:17:00 +0530

    first version

---
+++ download_screens.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop"
+mkdir -Force "Screens"
+
+Write-Host "Downloading Menu..."
+curl.exe -s -L -o "Screens/Menu.html" "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzAwMDY1MzhjODI1MDUyZDUwMzM4NWNkMzdhMDYxYWFhEgsSBxDrrsbjwwEYAZIBJAoKcHJvamVjdF9pZBIWQhQxNTAwNDI5Mjk1MDY3MjQxNTU5MQ&filename=&opi=89354086"
+curl.exe -s -L -o "Screens/Menu.png" "https://lh3.googleusercontent.com/aida/AP1WRLtgMMFT3v2JTv1mCx33L6TxaTEBlO1YCNaVd866lNaOPHubz7mUfoHpqNPt8OUJpqDRKYU9ai4TJyZH0oswgYzvT-kc-8m6pikpnEcm69D1Vw385AACh_pXp-vwoSGDH1Crp2qKBr3kcrFFbglv_c_STE8zKsBEI91X3ErRoCxxg-MqnMPox9kl_xz3D4lgcbOKVN558brT25G1KDD4qkm1BOkfz0T11-QuMSopyS_5d2MSBq6gy2d91Pei"
+
+Write-Host "Downloading Contact..."
+curl.exe -s -L -o "Screens/Contact.html" "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzAwMDY1MzhjODIyMjVjNTYwMzM4NWNkMzdhMDYxYWFhEgsSBxDrrsbjwwEYAZIBJAoKcHJvamVjdF9pZBIWQhQxNTAwNDI5Mjk1MDY3MjQxNTU5MQ&filename=&opi=89354086"
+curl.exe -s -L -o "Screens/Contact.png" "https://lh3.googleusercontent.com/aida/AP1WRLsJjm4PJMQTfQrMaEOykaZOUYUUBnPCqdUYMuZRj0NkwTilmeYX5tjd8gsocU_6etROII4gA6Dq3hzKnw0F2s0fEZH2wnxHYiLdMc5kkuZEgS-pS4tm4ehZGo5GrvQD8SPMCQ-hzuzpW3h-hlK12AcuXlXNxh2bxWr80iVw44PDeMYvFhrNGaUIS1FdZ-1ffiLAjyCtQeDI8f1jK57mxRcFGNvBLG3aIvBYYKAeipEHwbZf9U12FS1snf8"
+
+Write-Host "Downloading About Us..."
+curl.exe -s -L -o "Screens/About_Us.html" "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzAwMDY1MzhjODI5Yzk4MmUwMzMyZjYzZWM2MTBhYjZhEgsSBxDrrsbjwwEYAZIBJAoKcHJvamVjdF9pZBIWQhQxNTAwNDI5Mjk1MDY3MjQxNTU5MQ&filename=&opi=89354086"
+curl.exe -s -L -o "Screens/About_Us.png" "https://lh3.googleusercontent.com/aida/AP1WRLvWZ57b5eRNACixl6ZHGwT7P-czye69g2EFBGwk-Es4vu7PtiqrB1cD4GXL9fAGqE00XQEmFwt5RGQXJ3XdBLY3DGgGgJ_eTy1RWH0YzZaJue5jnbNaSqs2SAhpWDGFYKrgAO_Ks1ifnVYyQeCBNJgwcyQ2SHJJd6lB2VNb7TRaNe6bBo2jtIrx4f1CwM8WgST7ACfaXlHMNaFTCfgZezNKceit0A2YpePrWGkXdByBwOk16CycXaABlo0"
+
+Write-Host "Downloading Order Online..."
+curl.exe -s -L -o "Screens/Order_Online.html" "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzAwMDY1MzhjODIzZWIzNGUwODhlYmJlMzZiMTlhOThhEgsSBxDrrsbjwwEYAZIBJAoKcHJvamVjdF9pZBIWQhQxNTAwNDI5Mjk1MDY3MjQxNTU5MQ&filename=&opi=89354086"
+curl.exe -s -L -o "Screens/Order_Online.png" "https://lh3.googleusercontent.com/aida/AP1WRLu1JH1QgdrQOhhyeazZ4m3VRlipG0hQeb6KN91erQCTJrujt6OSBi6ftsNlc6xeN39vVLWKM2fXAeBiRDZbfrMrROOlP4QTCnZX8ZnjiwuPT-w9vGUvs6YRSuMXNYRY-U_1y-r0uBFHJkMJypChd0Hd2EXT9tdYPYyVkQA03ERiNL68B-3hPSEhzFEzDbFMp30E417SYXAHzbr95jO6hpxV5Unj20cdVgfgNObTK0kgyBOo-szDZHdsKUev"
+
+Write-Host "Downloading Home..."
+curl.exe -s -L -o "Screens/Home.html" "https://contribution.usercontent.google.com/download?c=CgthaWRhX2NvZGVmeBJ8Eh1hcHBfY29tcGFuaW9uX2dlbmVyYXRlZF9maWxlcxpbCiVodG1sXzAwMDY1MzhjODJhOGY1ZDUwNTAzYzJlNTM4MzE2ZWEyEgsSBxDrrsbjwwEYAZIBJAoKcHJvamVjdF9pZBIWQhQxNTAwNDI5Mjk1MDY3MjQxNTU5MQ&filename=&opi=89354086"
+curl.exe -s -L -o "Screens/Home.png" "https://lh3.googleusercontent.com/aida/AP1WRLsru67Tv9gK9DygeK5dB58kybJlJ2937gBG4jY8Y8YJMq8pkVa_c154lpKShqyMjfqbomA_e_BfFWxTlH4HRoTmUB2HX8WoCPLil3fttPSIDexonQzyzs_k-3Odz_TF7tUyz2_5VItnu-x-jlFew6KAS79PX_MZEejkJK_d9P7isDE0pDUjbsSilVdsNeLktTBGXCNvIZm5OsETTCb26MNuJrXTX9_JYK4Mybnv9t_ze_Fs62h3fXv7U7HE"
+
+Write-Host "All downloads complete."
